2048f8a007e8a28b55e24b3b776becd4d6292b72
jedisty <jedisty@hanyang.ac.kr>
2014-12-04 10:23:20 +0900

    Correct Typo mistake

--- FIRMWARE/firm_buffer_manager.c
+++ FIRMWARE/firm_buffer_manager.c
@@ -1,5 +1,5 @@
 // File: firm_buffer_manager.c
-// Data: 2014. 12. 03.
+// Date: 2014. 12. 03.
 // Author: Jinsoo Yoo (jedisty@hanyang.ac.kr)
 // Copyright(c)2014
 // Hanyang University, Seoul, Korea
