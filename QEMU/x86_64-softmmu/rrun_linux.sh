b8483a9d74d707a64b2be1dc47ee069afd4b1ef2
jedisty <jedisty@hanyang.ac.kr>
2017-11-10 20:51:45 +0900

    Modify the HDA file name

--- QEMU/x86_64-softmmu/rrun_linux.sh
+++ QEMU/x86_64-softmmu/rrun_linux.sh
@@ -3,4 +3,4 @@
 ## Hanyang University, Seoul, Korea
 ## Embedded Software Systems Laboratory. All right reserved
 
-./qemu-system-x86_64 -m 2048 -cpu core2duo -hda /mnt/rd/ssd.img -cdrom ../../OS/ubuntu-10.04.1-desktop-amd64.iso -usbdevice tablet
+./qemu-system-x86_64 -m 2048 -cpu core2duo -hda ../../RAMDISK/rd/ssd_hda.img -cdrom ../../OS/ubuntu-10.04.1-desktop-i386.iso -usbdevice tablet
